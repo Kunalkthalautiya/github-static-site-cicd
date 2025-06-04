bca491e384e588083edcbda8615c61ded77c2fb7
Thalautiya <152262114+Kunalkthalautiya@users.noreply.github.com>
2025-06-05 05:07:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// When the "Get Started" button is clicked
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector(".hero button");
-
-  button.addEventListener("click", () => {
-    alert("Thank you for your interest! We’ll contact you soon.");
-    console.log("Get Started button clicked");
-  });
-});
